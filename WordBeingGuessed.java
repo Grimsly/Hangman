3f97890c28713a7c48caced1feaa26dee168abe0
Grimsly <xmeng.low@gmail.com>
2017-10-20 01:37:54 -0600

    Changed some switch statements
    Cleaned up some switch statements at WordBeingGuessed

--- WordBeingGuessed.java
+++ WordBeingGuessed.java
@@ -3,6 +3,59 @@
 import java.util.Random;
 /**takes a random value from between 0 and 5 and then returns a word based on the number and the difficulty setting*/
 
+
+abstract class Difficulty{
+	public abstract String wordChosen(int wordNum);
+}
+
+class Easy extends Difficulty{
+	public String wordChosen(int wordNum) {
+		if (wordNum == 0) {
+			return "aimed";
+		} else if (wordNum == 1) {
+			return "havoc";
+		} else if (wordNum == 2) {
+			return "flute";
+		} else if (wordNum == 3) {
+			return "twist";
+		} else{
+			return "blade";
+		}
+	}
+}
+
+class Medium extends Difficulty{
+	public String wordChosen(int wordNum) {
+		if (wordNum == 0) {
+			return "girlfriend";
+		} else if (wordNum == 1) {
+			return "attractive";
+		} else if (wordNum == 2) {
+			return "resounding";
+		} else if (wordNum == 3) {
+			return "speciality";
+		} else{
+			return "xenophobia";
+		}
+	}
+}
+
+class Hard extends Difficulty{
+	public String wordChosen(int wordNum) {
+		if (wordNum == 0) {
+			return "counterargument";
+		} else if (wordNum == 1) {
+			return "autobiographies";
+		} else if (wordNum == 2) {
+			return "destructiveness";
+		} else if (wordNum == 3) {
+			return "logarithmically";
+		} else{
+			return "unsportsmanlike";
+		}
+	}
+}
+
 public class WordBeingGuessed {
 	//instance variables
 	private int dif;
@@ -20,66 +73,17 @@ public class WordBeingGuessed {
 	/**Depending on the difficulty level, a word will be randomly chosen for each difficulty
 	*@returns word*/
 	public String wordDifficulty(){
-
+		Difficulty howHard;
 		switch (dif) {
 			case 1:
-				switch (wordNum) {
-					case 0:
-						word = "aimed";
-						break;
-					case 1:
-						word = "havoc";
-						break;
-					case 2:
-						word = "flute";
-						break;
-					case 3:
-						word = "twist";
-						break;
-					case 4:
-						word = "blade";
-						break;
-				}
-				break;
+				howHard = new Easy();
+				word = howHard.wordChosen(wordNum);
 			case 2:
-				switch (wordNum) {
-				case 0:
-					word = "girlfriend";
-					break;
-				case 1:
-					word = "attractive";
-					break;
-				case 2:
-					word = "resounding";
-					break;
-				case 3:
-					word = "speciality";
-					break;
-				case 4:
-					word = "xenophobia";
-					break;
-				}
-				
-				break;
+				howHard = new Medium();
+				word = howHard.wordChosen(wordNum);
 			case 3:
-				switch (wordNum) {
-				case 0:
-					word = "counterargument";
-					break;
-				case 1:
-					word = "autobiographies";
-					break;
-				case 2:
-					word = "destructiveness";
-					break;
-				case 3:
-					word = "logarithmically";
-					break;
-				case 4:
-					word = "unsportsmanlike";
-					break;
-				}
-				break;
+				howHard = new Hard();
+				word = howHard.wordChosen(wordNum);
 		}
 		return word;
 	}
